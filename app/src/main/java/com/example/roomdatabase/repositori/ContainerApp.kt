6dc7e96b04a83f394eee93148a02d83c184446e4
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-12-06 22:32:55 +0700

    menambahkan ContainerApp

---
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase.repositori
+
+import android.content.Context
+import com.example.roomdatabase.data.DatabaseSiswa
+
+interface ContainerApp{
+    val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context): ContainerApp{
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositorisiswa(DatabaseSiswa.getDatabase(context).siswaDao())
+    }
+}
