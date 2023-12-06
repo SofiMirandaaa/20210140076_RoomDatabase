18256cbbbdf088401a16e93183fa483d0feb8f61
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-12-06 21:52:38 +0700

    menambahkan database utk table (siswa)

---
+++ app/src/main/java/com/example/roomdatabase/data/Siswa.kt
@@ -0,0 +1,15 @@
+package com.example.roomdatabase.data
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName ="tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
