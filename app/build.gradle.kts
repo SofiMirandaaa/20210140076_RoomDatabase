7aa8bec20a189cf0150290381bacfa61e1cd2781
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-12-06 23:18:48 +0700

    mengedit gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -74,5 +74,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
 
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
 }
