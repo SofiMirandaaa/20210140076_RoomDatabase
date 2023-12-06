a5f95704882a81fc0b663dac26abdb8ff6fa80ad
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-12-06 23:58:16 +0700

    mengedit main activy

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -10,7 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.roomdatabase.ui.theme.RoomDatabaseTheme
+import com.example.roomdatabase.navigasi.SiswaApp
+import com.example.roomdatabase.ui.halaman.theme.RoomDatabaseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +23,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SiswaApp()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RoomDatabaseTheme {
-        Greeting("Android")
-    }
 }
